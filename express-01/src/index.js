f2d69d348caf411c5efb9e4d42b32263eaddd6d2
Paulo Barreto Neto <105065779+PauloBarretoDev@users.noreply.github.com>
2023-09-15 22:11:50 +0000

    adições

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -3,89 +3,41 @@ import express from 'express';
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
 
-console.log("Projeto com NODE + Express - 2023.2");
-//console.log("PATH", process.env.PATH);
-console.log("MY_SECRET",process.env.MY_SECRET);
+import { v4 as uuidv4 } from 'uuid';
+import { Router } from 'express';
 
-let users = {
-    1: {
-      id: '1',
-      username: 'Robin Wieruch',
-    },
-    2: {
-      id: '2',
-      username: 'Dave Davids',
-    },
-  };
-  
-  let messages = {
-    1: {
-      id: '1',
-      text: 'Hello World',
-      userId: '1',
-    },
-    2: {
-      id: '2',
-      text: 'By World',
-      userId: '2',
-    },
+const router = Router();
+
+router.get('/', (req, res) => {
+  return res.send(Object.values(req.context.models.messages));
+});
+
+router.get('/:messageId', (req, res) => {
+  return res.send(req.context.models.messages[req.params.messageId]);
+});
+
+router.post('/', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.context.me.id,
   };
 
-const app = express();
+  req.context.models.messages[id] = message;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cors());
-
-app.get("/", (req,res) => {
-    res.send("Hello GET");
-});
-app.get('/messages', (req, res) => {
-    return res.send(Object.values(messages));
-  });
-  
-app.get('/messages/:messageId', (req, res) => {
-    return res.send(messages[req.params.messageId]);
+  return res.send(message);
 });
 
-app.post('/messages', (req, res) => {
-    const id = uuidv4();
-    const message = {
-      id,
-      text: req.body.text,
-    };
-  
-    messages[id] = message;
-  
-    return res.send(message);
-  });
+router.delete('/:messageId', (req, res) => {
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = req.context.models.messages;
 
-app.get('/users', (req, res) => {
-    return res.send(Object.values(users));
-});
-  
-app.get('/users/:userId', (req, res) => {
-    return res.send(users[req.params.userId]);
-});
-app.post('/users', (req, res) => {
-    return res.send('POST HTTP method on user resource');
-});
-  
-app.put('/users/:userId', (req, res) => {
-    return res.send(
-      `PUT HTTP method on user/${req.params.userId} resource`,
-    );
-});
-  
-app.delete('/users/:userId', (req, res) => {
-    return res.send(
-      `DELETE HTTP method on user/${req.params.userId} resource`,
-    );
-});  
+  req.context.models.messages = otherMessages;
 
+  return res.send(message);
+});
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => 
-    console.log(`App test listening on port ${port}`)
-);
+export default router;
